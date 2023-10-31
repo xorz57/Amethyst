33517c605733ea4ceb700dff6af53705b4f145d0
George Fotopoulos <xorz57@gmail.com>
2023-11-01 01:23:15 +0200

    Remove Amethyst.hpp

--- Engine/include/Amethyst.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Amethyst/Application.hpp"
-#include "Amethyst/ApplicationSettings.hpp"
-#include "Amethyst/Layer.hpp"
-#include "Amethyst/LayerStack.hpp"
-#include "Amethyst/Logging.hpp"
